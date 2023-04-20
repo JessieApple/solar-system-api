3b1dcc3d8df358f3867b072ad4e397d37a7a3d1a
Jessie <wajliu3@gmail.com>
2023-04-20 11:24:31 -0700

    Create planet class

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,10 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self,id,name,description,has_humans=False):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.has_humans = has_humans 
+        
+    
