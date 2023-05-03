5e3ec72ece6bba5b7070eabaa26da811fd0ce748
Jessie <wajliu3@gmail.com>
2023-05-03 13:57:24 -0700

    create get one planet test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,39 @@ def test_get_all_planets_with_no_records(client):
     
     assert response.status_code == 200
     assert response_body == []
+    
+def test_get_all_planets_with_records(client,two_planets):
+    response = client.get('/planets')
+    response_body = response.get_json()
+    
+    assert response.status_code == 200
+    assert response_body == [{
+        'id':1,
+        'name':"Earth", 
+        'description':"Kinda cool", 
+        'has_humans':True
+        },
+        {
+        'id':2,
+        'name':"Pluto", 
+        'description':"It's a planet", 
+        'has_humans':False 
+        }]
+    
+def test_get_one_planet(client,two_planets):
+    response = client.get('/planets/1')
+    response_body = response.get_json()
+    
+    assert response.status_code == 200
+    assert response_body == {
+        'id':1,
+        'name':"Earth", 
+        'description':"Kinda cool", 
+        'has_humans':True
+        }
+    
+def test_get_planet_not_exist(client):
+    response = client.get('/planets/1')
+    
+    assert response.status_code == 404
+    assert "Not Found" in response 
