127ac79c54305946a66106a278dfda7b641ad7d8
Jessie <wajliu3@gmail.com>
2023-05-03 13:40:55 -0700

    first test with no records

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest 
 from app import create_app, db
-from .models.planet import Planet
+from app.models.planet import Planet
 from flask.signals import request_finished
 
 @pytest.fixture
